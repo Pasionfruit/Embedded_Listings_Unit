3fce5440b07219560a0927a9be4060d6f6ab1696
Abe Pasion <pasionabe@gmail.com>
2025-02-18 13:36:47 -0500

    restarts project

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ app.get('/', (req, res) => {
 });
 
 
-// const PORT = 5000;
-// app.listen(PORT, "0.0.0.0", () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
+const PORT = 5000;
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
 // const { Client } = require('pg');
 
