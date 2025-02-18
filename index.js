4df53a5bdc37c2df5e8ce2c27fe2a289464cbc2a
Abe Pasion <pasionabe@gmail.com>
2025-02-18 13:13:18 -0500

    restart project

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+app.get('/', (req, res) => {
+    res.send('Frontend');
+});
+
+
+// const PORT = 5000;
+// app.listen(PORT, "0.0.0.0", () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
+
+// const { Client } = require('pg');
+
+// const client = new Client({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT
+// });
+
+// client.connect()
+//     .then(() => console.log("Connected to PostgreSQL"))
+//     .catch(err => console.error("Connection error", err));
