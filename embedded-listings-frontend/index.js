541aa2c6d373ef38f3b06032932e6a1d52dccacf
Abe Pasion <pasionabe@gmail.com>
2025-02-18 12:09:20 -0500

    installs concurrency

--- embedded-listings-frontend/index.js
+++ embedded-listings-frontend/index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     res.send('This is a test');
 });
 
-const PORT = 3000;
+const PORT = 5000;
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
