a0211d9826bd829fb1276f7ffbc3b1048710b564
Abe Pasion <pasionabe@gmail.com>
2025-02-20 10:40:13 -0500

    updates server.js to run https

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,34 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Read SSL certificates
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/unitzo.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/unitzo.com/fullchain.pem', 'utf8');
+
+// Create credentials object
+const credentials = { key: privateKey, cert: certificate };
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.get('/api/your-api', (req, res) => {
-    res.json({message: 'Hello from backend!' });
+    res.json({ message: 'Hello from backend!' });
 });
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Server running on http://0.0.0.0:${port}`)
-})
+// Create HTTPS server
+https.createServer(credentials, app).listen(443, () => {
+    console.log('HTTPS Server running on https://unitzo.com');
+});
+
+// Optionally, create HTTP server to redirect to HTTPS
+http.createServer(app).listen(80, () => {
+    console.log('HTTP Server running and redirecting to HTTPS');
+});
